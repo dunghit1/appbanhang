998a7f8c08b9d84689fe6c823ff83cbd72ee5774
dunghit22 <dungnguyenhit09@gmail.com>
2021-08-17 18:29:40 +0700

    comment

---
+++ login/Registration.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function Registration({navigation}){
+    <View>
+        <Text>12</Text>
+    </View>
+}
+export default Registration;
